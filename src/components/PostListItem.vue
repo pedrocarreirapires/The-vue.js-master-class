03a31fa5f77c9c6b9d1ae70e5b7378becbd6f056
Pedro Pires <aluno21215@ipt.pt>
2023-02-01 11:45:27 +0000

    Conditionally display multiple elements with an invisible wrapper

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -11,16 +11,24 @@
 
 
     <div class="post-content">
-      <div v-if="!editing">
-        {{post.text}}
-      </div>
 
+      <template v-if="!editing">
+        <div>
+          {{post.text}}
+        </div>
+        <a @click.prevent="editing = true" href="#" style="margin-left: auto;" class="link-unstyled" title="Make a change">
+          <i class="fa fa-pencil"></i>
+        </a>
+      </template>
       <div v-else>
       <PostEditor
         :post="post"
         @save="editing = false"
       />
       </div>
+
+
+
     </div>
     <div class="post-date text-faded">
         <AppDate :timestamp="post.publishedAt"/>
