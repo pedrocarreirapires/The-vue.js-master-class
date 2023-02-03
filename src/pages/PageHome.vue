23933acb1fac2e028a9c76a23e4dcc1c2ec70690
Pedro Pires <aluno21215@ipt.pt>
2023-02-03 15:27:52 +0000

    Hide Content Until Data is Fully Loaded

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-full push-top">
+  <div v-if="ready" class="col-full push-top">
     <h1>Welcome to the Forum</h1>
     <CategoryList :categories="categories"/>
   </div>
@@ -14,6 +14,13 @@ export default {
   components: {
     CategoryList
   },
+
+  data () {
+    return {
+      ready: false
+    }
+  },
+
   computed: {
     categories () {
       return Object.values(this.$store.state.categories)
@@ -25,7 +32,10 @@ export default {
   created () {
     this.fetchAllCategories()
       .then(categories => {
-        categories.forEach(category => this.fetchForums({ids: Object.keys(category.forums)}))
+        Promise.all(categories.map(category => this.fetchForums({ids: Object.keys(category.forums)})))
+        .then(() => {
+          this.ready = true
+        })
       })
   },
   beforeMount () {
