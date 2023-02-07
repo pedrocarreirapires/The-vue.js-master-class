252f427c4674f4ff2a80a59e9a7ccba7ef16b052
Pedro Pires <aluno21215@ipt.pt>
2023-02-07 13:12:11 +0000

    Using .env file in the Vue.js Webpack Template

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,12 +1,15 @@
 'use strict'
+
+require('dotenv').config()
+
 module.exports = {
-  FIREBASE_NODE_ENV: '"production"',
-  FIREBASE_APIKEY: '"AIzaSyDN0DEnZG4d7iSeLvTUF4uUoW28R5F00yI"',
-  FIREBASE_AUTH_DOMAIN: '"vueschool-9b264.firebaseapp.com"',
-  FIREBASE_DATA_BASE_URL: '"https://vueschool-9b264-default-rtdb.firebaseio.com"',
-  FIREBASE_PROJECT_ID: '"vueschool-9b264"',
-  FIREBASE_STORAGE_BUCKET: '"vueschool-9b264.appspot.com"',
-  FIREBASE_MESSAGING_SENDER_ID: '"278870588587"',
-  FIREBASE_APP_ID: '"1:278870588587:web:6147ea750ac4ee12710bee"'
+  NODE_ENV: "'production'",
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+  FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+  FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+  FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+  FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+  FIREBASE_MESSAGING_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_ID),
+  FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID)
 }
 
--- src/main.js
+++ src/main.js
@@ -15,10 +15,10 @@ Vue.config.productionTip = false
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATA_BASE_URL,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
   appId: process.env.FIREBASE_APP_ID
 }
 firebase.initializeApp(firebaseConfig)
